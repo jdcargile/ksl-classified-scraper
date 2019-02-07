47c4cf7df635a781837a0f41eadd3d940d17a3a3
jcargile@control4.com <jcargile@control4.com>
2019-02-06 23:08:23 -0700

    New KSL Scaper solution

---
+++ ksl-classifieds-scraper/Controllers/SearchController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace ksl_classifieds_scraper.Controllers
+{
+    [Route("api/[controller]")]
+    public class SearchController : Controller
+    {
+        // GET api/search/nintendo+switch
+        [HttpGet("{query}")]
+        public async Task<string> GetAsync(string query)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var address = $"https://classifieds.ksl.com/search?category[]=&subCategory[]=&keyword={query}&priceFrom=&priceTo=&zip=&miles=25&sellerType[]=Private&marketType[]=Sale&hasPhotos[]=Has%20Photos&postedTimeFQ[]=&perPage=1000";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(address);
+
+            //Grab items array from script tag and delete surround JS method text to serialize into JSON object
+            var itemsScript = document.QuerySelectorAll("*").FirstOrDefault(m => m.LocalName == "script" && m.InnerHtml.Contains("window.renderSearchSection("));
+            var itemsJson = itemsScript.InnerHtml.Replace("window.renderSearchSection(", "");
+            var items = itemsJson.Replace("kslBaseDomain: 'ksl.com',\n                })", "}");
+            var json = JObject.Parse(items);
+
+            //Determine whether there are more pages. Ksl only grabs a max of 96 items at a time.
+            var pagination = document.QuerySelectorAll("*").FirstOrDefault(m => m.ClassName == "pagination").Children.Where(m => m.ClassName == "link");
+
+
+            foreach(var page in pagination)
+            {
+                var href = page.GetAttribute("href");
+                var additionalPage = await context.OpenAsync(address);
+                var additionalPageScript = document.QuerySelectorAll("*").FirstOrDefault(m => m.LocalName == "script" && m.InnerHtml.Contains("window.renderSearchSection("));
+                var additionalJson = itemsScript.InnerHtml.Replace("window.renderSearchSection(", "");
+                var additionalItems = itemsJson.Replace("kslBaseDomain: 'ksl.com',\n                })", "}");
+                var additionalItemJson = JObject.Parse(additionalItems);
+                json.Merge(additionalItemJson);
+            }
+
+            return json.ToString();
+
+        }
+    }
+}
---
+++ ksl-classifieds-scraper/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ksl_classifieds_scraper
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "KSL Scraper API", Version = "v1" });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+            );
+
+            app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "KSL Scraper API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+        }
+    }
+}
