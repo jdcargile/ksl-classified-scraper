71159ff13add6499fdd23396d311140fe38adc15
jcargile@control4.com <jcargile@control4.com>
2019-12-22 15:41:38 -0700

    Add Get Listing Support
    Adding get listing support

---
+++ ksl-classifieds-scraper/Helpers/StringHelpers.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ksl_classifieds_scraper.Helpers.StringHelpers
+{
+    public class StringHelpers
+    {
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
